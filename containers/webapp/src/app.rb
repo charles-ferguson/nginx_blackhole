a3e7430edaef7b31a17b16ddad9447d3ece7a858
Charles Ferguson <charles.ferguson@clio.com>
2020-11-12 20:24:25 -0800

    Enable killswitching lua message.

--- containers/webapp/src/app.rb
+++ containers/webapp/src/app.rb
@@ -2,9 +2,22 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'fileutils'
 
 set :bind, '0.0.0.0'
 set :port, 4567
+
+LUA_KILLSWITCH = './killswitches/uri/lua'
+post '/kill/lua' do
+  dirname = File.dirname(LUA_KILLSWITCH)
+
+  FileUtils.mkdir_p(dirname) unless File.directory?(dirname)
+  FileUtils.touch(LUA_KILLSWITCH)
+end
+
+delete '/kill/lua' do
+  File.unlink(LUA_KILLSWITCH) if File.exist?(LUA_KILLSWITCH)
+end
 
 get '/*' do
   "Hi, you hit: #{request.path_info}"
